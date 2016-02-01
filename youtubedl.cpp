1ff100b39b57111b1836388eeda8a949cde4f11e
rrooij <rderooij685@gmail.com>
2016-02-01 10:39:59 +0100

    YoutubeDL: use flat playlist json
    Don't extract information from every single video. It makes format
    selection very slow to load.

--- youtubedl.cpp
+++ youtubedl.cpp
@@ -26,7 +26,7 @@ YoutubeDL::YoutubeDL()
 
 QJsonObject YoutubeDL::createJsonObject(QString url)
 {
-    arguments << "-j" << "--no-playlist" << url;
+    arguments << "-j" << "--no-playlist" << "--flat-playlist" << url;
     ytdl->setProcessChannelMode(QProcess::SeparateChannels);
     ytdl->start(this->program, this->arguments);
     ytdl->waitForFinished();
